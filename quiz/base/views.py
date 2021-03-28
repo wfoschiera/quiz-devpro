9ca3fa7f80628c001d7196237c9cd66927d85d99
wfoschiera <wfoschiera@gmail.com>
2021-03-28 15:17:03 -0400

    finalizada aula 2, implementado loop perguntas no template

--- quiz/base/views.py
+++ quiz/base/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from quiz.base.models import Pergunta
+
+
+def home(request):
+    return render(request, 'base/home.html')
+
+
+def perguntas(request, indice):
+    pergunta = Pergunta.objects.filter(disponivel=True).order_by('id')[indice - 1]
+    ctx = { 'indice_da_questao': indice, 'pergunta': pergunta }
+    return render(request, 'base/game.html', context=ctx)
+
+
+def classificacao(request):
+    return render(request, 'base/classificacao.html')
