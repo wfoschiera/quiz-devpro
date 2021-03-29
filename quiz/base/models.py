d65eef9eb87b71eb9492a767126366db33c627c3
wfoschiera <wfoschiera@gmail.com>
2021-03-29 08:52:58 -0400

    criado modelo de aluno, incluido tratamento de caso de usuario inexistente e redirecionamento de usuario existente

--- quiz/base/models.py
+++ quiz/base/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Aluno(models.Model):
+    nome = models.CharField(max_length=64)
+    email = models.EmailField(unique=True)
+    criado_em = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome
+
+
+
 class Pergunta(models.Model):
     enunciado = models.TextField()
     disponivel = models.BooleanField(default=False)
--- quiz/base/views.py
+++ quiz/base/views.py
@@ -1,16 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
-from quiz.base.models import Pergunta
+from quiz.base.forms import AlunoForm
+from quiz.base.models import Pergunta, Aluno
 
 
 def home(request):
+    if request.method == "POST":
+        # Usuário já existe
+        email = request.POST['email']
+        try:
+            aluno = Aluno.objects.get(email=email)
+        except Aluno.DoesNotExist:
+            # Usuário não existe
+            formulario = AlunoForm(request.POST)
+            if formulario.is_valid():
+                aluno = formulario.save()
+                request.session['aluno_id'] = aluno.id
+                return redirect('/perguntas/1')
+            else:
+                contexto = {'formulario': formulario}
+                return render(request, 'base/home.html', context=contexto)
+        else:
+            request.session['aluno_id'] = aluno.id
+            return redirect('/perguntas/1')
     return render(request, 'base/home.html')
 
 
 def perguntas(request, indice):
     pergunta = Pergunta.objects.filter(disponivel=True).order_by('id')[indice - 1]
-    ctx = { 'indice_da_questao': indice, 'pergunta': pergunta }
+    ctx = {'indice_da_questao': indice, 'pergunta': pergunta}
     return render(request, 'base/game.html', context=ctx)
 
 
